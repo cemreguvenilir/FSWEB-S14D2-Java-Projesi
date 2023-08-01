76c7da4a4d86abf3a9e718d498ed793bbfb82532
cemreguvenilir <guvenilircemre@gmail.com>
2023-08-01 15:10:40 +0300

    room harici yapıldı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Bed bed1 = new Bed("double", 4, 180, 1, 2);
+        Wall wall1 = new Wall("north");
+        Wall wall2 = new Wall("south");
+        Wall wall3 = new Wall("east");
+        Wall wall4 = new Wall("west");
+
+        Lamp lamp1 = new Lamp(LampType.LED, true, 15);
+        Wardrobe wardrobe1 = new Wardrobe(170, 180, 30.5);
+        Carpet carpet1 = new Carpet(100, 130, PaintColor.PINK);
+        Bedroom bedroom = new Bedroom("Cemre's bedroom", wall1, wall2, wall3, wall4, new Ceiling(200, PaintColor.BLUE), bed1, lamp1, wardrobe1, carpet1);
+        System.out.println(bedroom);
+        bedroom.getCarpet().lying();
+        bedroom.getBed().make();
+        bedroom.getName();
+        bedroom.getCeiling().create();
+        bedroom.getWall1().create();
+        bedroom.getLamp().turnOn();
+        bedroom.getWardrobe().add();
+
     }
 }
---
+++ src/model/Wall.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+    public void create(){
+        System.out.println(getDirection());
+    }
+
+    @Override
+    public String toString() {
+        return "Wall{" +
+                "direction='" + direction + '\'' +
+                '}';
+    }
+}
